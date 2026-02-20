be16a0b02bd41244e3eaba049bbbb5ddf297c219
Olayiwola Theophilus <Olayiwolatheophilusayomide@gmail.com>
2026-02-20 07:11:29 +0100

    create folder structure template and add the folders

---
+++ template.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "setup.py",
+    ".env"
+    "expreiments/experiments.ipynb"
+    "app.py",
+    "store_index.py",
+    "static/.gitkeep",
+    "templates/chat.html"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not filepath.exists()) or (filename.get_size == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"File already exists: {filepath}")
